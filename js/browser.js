ac17f9bb684770a5641b9a2127f95eac54807de1
Dogboy21 <dogboy2103@gmail.com>
2019-03-18 02:39:42 +0100

    Added Pagination and more categories

--- js/browser.js
+++ js/browser.js
@@ -1,23 +1,34 @@
-document.getElementById('name-input').addEventListener('keyup', function (event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-        doSearch();
-    } 
+$(document).ready(function() {
+    document.getElementById('name-input').addEventListener('keyup', function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            doSearch();
+        } 
+    });
+    
+    showObjects(false, true);
 });
-
-showObjects(false, true);
 
 function setCategoryMenu() {
     var categories = getUniqueCategories();
+    var categoryNames = Object.keys(categories);
+    categoryNames.sort();
     var dropdown = document.getElementById('category-dropdown');
-    for (var i = 0; i < categories.length; i++) {
-        var category = categories[i];
+    for (var i = 0; i < categoryNames.length; i++) {
+        var category = categoryNames[i];
+        var count = categories[category];
         var link = document.createElement('a');
         link.setAttribute('class', 'dropdown-item clickable');
-        link.innerHTML = category;
+        link.appendChild(document.createTextNode(`${category} (${count})`));
         link.addEventListener('click', browseToCategory(category));
         dropdown.appendChild(link);
     }
+}
+
+function findWithoutCategory() {
+    showObjects(function(obj) {
+        return !window.categories[obj[1]];
+    });
 }
 
 function browseToCategory(category) {
@@ -50,23 +61,6 @@ function doSearch() {
 
 function getPreviewForObject(object, small) {
     return `https://cdn.rage.mp/public/odb/imgs${small ? '-small' : ''}/${object[1]}-${object[0]}.jpg`;
-}
-
-function showObjects(filter, defaultView) {
-    var content = document.getElementById('content');
-    while (content.firstChild) {
-        content.removeChild(content.firstChild);
-    }
-
-    var row = document.createElement('row');
-    row.setAttribute('class', 'row');
-
-    (defaultView ? window.objects.slice(0, 100) : window.objects.filter(filter)).forEach(function(obj) {
-        row.appendChild(createCardForObject(obj));
-    });
-
-    var content = document.getElementById('content');
-    content.appendChild(row);
 }
 
 function hasCategory(obj, category) {
@@ -143,9 +137,77 @@ function getUniqueCategories() {
 
     for (var i = 0; i < categoryArrays.length; i++) {
         for (var j = 0; j < categoryArrays[i].length; j++) {
-            categories[categoryArrays[i][j]] = "";
+            var old = categories[categoryArrays[i][j]] || 0
+            categories[categoryArrays[i][j]] = old + 1;
         }
     }
 
-    return Object.keys(categories);
+    return categories;
+}
+
+window.currentObjects = [];
+window.page = 0;
+var pageSize = 100;
+
+function showObjects(filter, defaultView) {
+    window.currentObjects = defaultView ? window.objects.slice(0, window.objects.length) : window.objects.filter(filter);
+    console.log(window.currentObjects.length + ' objects');
+    window.currentObjects.sort(function(obj1, obj2) {
+        return obj1[1] - obj2[1];
+    })
+
+    setPage(0);
+}
+
+function setPage(newPage) {
+    window.page = newPage;
+    var availPages = Math.ceil(currentObjects.length / pageSize);
+
+    var content = document.getElementById('content');
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+
+    var row = document.createElement('row');
+    row.setAttribute('class', 'row');
+
+    window.currentObjects.slice(window.page * pageSize, (window.page + 1) * pageSize).forEach(function(obj) {
+        row.appendChild(createCardForObject(obj));
+    });
+
+    var content = document.getElementById('content');
+    content.appendChild(row);
+
+    var heading = document.createElement('small');
+    heading.appendChild(document.createTextNode(`Page ${page + 1} of ${availPages}, ${currentObjects.length} total objects`));
+    content.appendChild(heading);
+
+    if (availPages > 0) {
+        if ((window.page + 1) < availPages) {
+            var btnNext = document.createElement('button');
+            btnNext.setAttribute('type', 'button');
+            btnNext.setAttribute('class', 'btn btn-sm btn-outline-secondary');
+            btnNext.innerHTML = 'Next Page';
+            btnNext.style.cssFloat = 'right';
+            btnNext.addEventListener('click', function() {
+                setPage(window.page + 1);
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+            })
+            content.appendChild(btnNext);
+        }
+
+        if (window.page > 0) {
+            var btnPrev = document.createElement('button');
+            btnPrev.setAttribute('type', 'button');
+            btnPrev.setAttribute('class', 'btn btn-sm btn-outline-secondary');
+            btnPrev.innerHTML = 'Previous Page';
+            btnPrev.style.cssFloat = 'right';
+            btnPrev.style.marginRight = '10px';
+            btnPrev.addEventListener('click', function() {
+                setPage(window.page - 1);
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+            })
+            content.appendChild(btnPrev);
+        }
+    }
 }
